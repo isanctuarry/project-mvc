05a40340a688fe0b9223819c113b325ce0c3744b
isanctuarry <khrnsa143@gmail.com>
2025-11-07 18:19:41 +0700

    ubah lagi

--- config/database.php
+++ config/database.php
@@ -5,6 +5,6 @@ return [
     'port' => getenv('MYSQLPORT') ?: '3306',
     'dbname' => getenv('MYSQLDATABASE') ?: 'mvc_db',
     'username' => getenv('MYSQLUSER') ?: 'root',
-    'password' => getenv('MYSQLPASSWORD') ?: 'Kangkitty30_',
+    'password' => getenv('MYSQLPASSWORD') ?: '',
     'charset' => 'utf8mb4'
 ];
